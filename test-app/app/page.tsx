5af92c8593c36869c40a5e80f114dbcf0e356f19
DanteSnow <clydehan0401@gmail.com>
2024-12-17 16:57:59 +0900

    테스트

--- test-app/app/page.tsx
+++ test-app/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and test see your test changes instantly.</li>
+          <li>Save and test see your test changes iasdasdasnstantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
